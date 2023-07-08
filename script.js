e607fd0f6d0034c822011fdfc5e2238d4da804e3
bourgu20al <bourgu20al@lycee-ndduroc.com>
2023-07-08 03:18:08 +0200

    Création d'un carousel slider + modification de la section queqlues images, formatage des images + changement d'animation sur hover

--- script.js
+++
@@ -1,6 +0,0 @@
-window.addEventListener('scroll', function() {
-    var bg = document.getElementById('background');
-    var yPos = -window.pageYOffset / 4; // Réglez cette valeur pour ajuster la vitesse de défilement
-
-    bg.style.backgroundPositionY = yPos + 'px';
-});
